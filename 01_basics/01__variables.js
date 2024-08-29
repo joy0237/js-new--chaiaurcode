6c88ead70d1906825f1b011a97acc710ccd3b825
joy0237 <138614238+joy0237@users.noreply.github.com>
2024-08-29 01:32:51 +0000

    done

---
+++ 01_basics/01__variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "joyshree.27.03.2k@gamil.com"
+var accountPassword ="12345"
+accountPlace = "Kolkata"
+
+// accountId = 2 //not allowed since accountId is constant
+
+accountEmial = "abc.123d2gmail.com"
+accountPassword = "98765"
+accountPlace = "Leh"
+
+console.log(accountId);
+console.table ([accountId, accountEmail, accountPassword, accountPlace])
