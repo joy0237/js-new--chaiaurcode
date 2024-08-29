8998bf7dfe674f9ea2684f28b645e80e3eaa04da
joy0237 <138614238+joy0237@users.noreply.github.com>
2024-08-29 02:10:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) we are using node js and not browser
+
+console.log(3+3)
+// code readibility should be high
+console.log("JOYJOY")
+
+let Name = "Joyshree"
+let Age = 24
+let LogIn = false
+let State
+
+/* primitive data types
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => used mainly in react for uniqueness */
+
+/*
+//object
+*/
+ console.log(typeof Age);
+ console.log(typeof null)
+ console.log(typeof undefined) 
