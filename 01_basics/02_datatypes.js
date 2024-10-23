05b21f2b974828a89373c8134636b4518bc9b460
Joyshree Paul <138614238+joy0237@users.noreply.github.com>
2024-10-23 00:02:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(3+3); not used in node directly but is used after being binded with HTML or in developer consol 
+
+//code readablity should be high
+console.log(3+3);  
+console.log("joyshree");
+
+let name = "joyshree";
+let age = 25;
+let isLoggedIn = false;
+let state;
+
+// number => ranges to 2 to the power 53
+// string => use in double quotes refereble can be used in single quotes
+// boolean => either ture or false
+// null => its a standalone value, it can be assigned
+// undefined => value not assigned
+// symbol => find uniqueness (used in react mostly)
+// object => 
+
+console.log(typeof age);
+console.log(typeof null); // null is an object
+console.log(typeof undefined); // undefined is undefined 
+
